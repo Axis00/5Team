b746945baff9b56d254c4b7fd058c2568f4adbdc
hyuzenn <huj22641@naver.com>
2024-08-16 22:51:30 +0900

    이 파일 실행하면 로컬호스트로 웹 실행

---
+++ server.py
@@ -0,0 +1,22 @@
+import http.server
+import socketserver
+import webbrowser
+import threading
+
+PORT = 8000
+DIRECTORY = "."
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+def start_server():
+    with socketserver.TCPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
+        print(f"Serving at http://localhost:{PORT}")
+        httpd.serve_forever()
+
+# Start the server in a separate thread
+threading.Thread(target=start_server).start()
+
+# Open the default browser to the specified file
+webbrowser.open(f'http://localhost:{8080}/music.html')
